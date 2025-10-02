42d6c8d25beb51a62f6354456e7e18f281894af9
ani <108333567+not-ani@users.noreply.github.com>
2025-10-01 20:42:25 -0600

    feat: added posthog

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,6 +8,19 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ocw-path-for-stuff/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ocw-path-for-stuff/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
   images: {
     //https://ugakd4mkxv.ufs.sh/f/QRXW6mPDvNgcf1bAHpXv5c4nkOatgwsmYj96KRpli3hUEdx1
     domains: ["ugakd4mkxv.ufs.sh"],
