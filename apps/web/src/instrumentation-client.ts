6ed91d4f7db739c22d0b00d3c66257beb32807bc
ani <108333567+not-ani@users.noreply.github.com>
2025-10-19 17:37:30 -0600

    fix: update PostHog configuration to use dynamic host and clear ui_host

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -1,7 +1,10 @@
 import posthog from "posthog-js";
 
+const host = process.env.NODE_ENV === "production" ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : `http://localhost:3001`;
+
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: `${host}/ocw-path-for-stuff`,
+  ui_host: `${host}/ocw-path-for-stuff`,
   defaults: "2025-05-24",
   person_profiles: "always",
 });
