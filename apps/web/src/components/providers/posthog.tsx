857de9255edb6653e1424ebf1abc3171bb0256aa
ani <108333567+not-ani@users.noreply.github.com>
2025-10-19 17:35:21 -0600

    fix: posthog host

--- apps/web/src/components/providers/posthog.tsx
+++ apps/web/src/components/providers/posthog.tsx
@@ -15,7 +15,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host:
         `${host}/ocw-path-for-stuff`,
-      ui_host: "https://us.posthog.com",
+      ui_host: `${host}/ocw-path-for-stuff`,
       person_profiles: "always",
       capture_pageview: true,
     });
